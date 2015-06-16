8099b44d1af866cd2c3c445b73e08714aa30a052
Firemark <marpiechula@gmail.com>
2015-06-17 01:15:55 +0200

    add argparse

--- homework_parser/__main__.py
+++ homework_parser/__main__.py
@@ -1,25 +1,40 @@
 from homework_parser.file_parser import detect_plugin
 
 from sys import argv, stdin, stdout, stderr, exit
+from argparse import ArgumentParser, FileType
+
+parser = ArgumentParser(description='parse file/stdint and write to file/stdout')
+parser.add_argument('in_format', help='plugin to load file/stdin')
+parser.add_argument('out_format', help='plugin to write to file/stdout')
+
+parser.add_argument(
+    '--infile',
+    nargs='?',
+    type=FileType('r'),
+    default=stdin,
+    help='file to load data (optional)'
+)
+parser.add_argument(
+    '--outfile',
+    nargs='?',
+    type=FileType('w'),
+    default=stdout,
+    help='file to save data (optional)'
+)
 
 if __name__ == "__main__":
-    in_format = argv[1]
-    out_format = argv[2]
-    out_plugin = detect_plugin(out_format)
+    args = parser.parse_args()
+    out_plugin = detect_plugin(args.out_format)
 
     if out_plugin is None:
-        print >> stderr, ('out-plugin %s not found' % out_format)
+        print >> stderr, ('out-plugin %s not found' % args.out_format)
         exit(-1)
 
-    in_plugin = detect_plugin(in_format)
+    in_plugin = detect_plugin(args.in_format)
     if in_plugin is None:
-        print >> stderr, ('in-plugin %s not found' % in_format)
+        print >> stderr, ('in-plugin %s not found' % args.in_format)
         exit(-1)
      
-    if len(argv) == 4:
-        with open(argv[3]) as f:
-            data = in_plugin.read_from_file(f)
-    else:
-        data = in_plugin.read_from_file(stdin)
-    
-    out_plugin.write_to_file(stdout, data)
+    data = in_plugin.read_from_file(args.infile)
+    out_plugin.write_to_file(args.outfile, data)
+
