6968b2d610ce1c9d1f6215695c205f92330a0b3e
Firemark <marpiechula@gmail.com>
2015-06-16 23:27:51 +0200

    write more tests

--- homework_parser/tests.py
+++ homework_parser/tests.py
@@ -1,5 +1,6 @@
 from plugin import AbstractPlugin
 from file_parser import detect_plugin
+from cStringIO import StringIO
 
 import pytest
 
@@ -13,4 +14,23 @@ def test_detect_plugin():
 def test_detect_unknown_plugin():
     plugin = detect_plugin('foobar')
     assert plugin is None
+
+
+def test_load_json():
+    plugin = detect_plugin('json')
+    stream = StringIO(
+        '[{"a": 0, "b": 1}, {"a": 0, "b": 1}]'
+    )
+    data = plugin.read_from_file(stream)
+    assert data == [{"a": 0, "b": 1}, {"a": 0, "b": 1}]
+
+
+def test_write_json():
+    plugin = detect_plugin('json')
+    data = [{"a": 0, "b": 1}, {"a": 0, "b": 1}]
+    stream = StringIO()
+    plugin.write_to_file(stream, data)
+    assert stream.getvalue() == (
+        '[{"a":0,"b":1},{"a":0,"b":1}]'
+    )
 
