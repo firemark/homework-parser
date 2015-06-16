768c21d42237faa045a62a653c987b3c389cf99f
Firemark <marpiechula@gmail.com>
2015-06-16 22:36:30 +0200

    add tests

---
+++ parser/tests.py
@@ -0,0 +1,16 @@
+from plugin import AbstractPlugin
+from file_parser import detect_plugin
+
+import pytest
+
+
+def test_detect_plugin():
+    plugin = detect_plugin('foo')
+    assert plugin.__name__ == 'FooPlugin'
+    assert isinstance(plugin, AbstractPlugin)
+    
+
+def test_detect_unknown_plugin():
+    plugin = detect_plugin('foobar')
+    assert plugin is None
+
